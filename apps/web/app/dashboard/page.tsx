a17714f4d487e290ce421c7284e7d9b75e7f8922
Chandra Sekhar <chandrasekhar4321n@gmail.com>
2026-05-04 15:53:28 +0530

    added the drawing logic

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -1,60 +1,65 @@
-"use client";
-
-import { useEffect , useState  } from "react";
-import { useSocket } from "../Context-API/UseSocket";
-
+"use client"; 
+import {useRef  , useState , useEffect, MouseEvent } from "react"
 export default function DashBoard() {
-  const { socket, connect, disconnect } = useSocket();
-
-  useEffect(() => {
-    connect();
-
-    return () => {
-      disconnect();
-    };
-  }, []);
-
-  useEffect(() => {
-    if (!socket) return;
-
-    socket.on("connect", () => {
-      console.log("Socket connected:", socket.id);
-    });
-
-    return () => {
-      socket.off("connect");
-    };
-  }, [socket]);
-  const SendMessage = ()=>{
-    console.log(text); 
-    console.log("function called ")
-    socket?.emit("message" , text) ; 
-    Settext("")
-  }
-  const [text ,Settext] = useState<string>(""); 
-  const [messages , Setmessages] = useState<string[]>([]) ; 
-  const SetOnchange = (t : React.ChangeEvent<HTMLInputElement>)=>{
-     Settext(t.target.value) ; 
-  }
-  socket?.on("message" , (data)=> {
-    Setmessages([...messages , data]) ; 
+  const [isDrawing ,SetisDrawing] = useState<boolean>(false); 
+  //staring point
+  const [points , SetPoints] = useState<{x : number , y : number }>({
+    x : 0 , 
+    y : 0 
   })
-  return <>
-    <div> 
-      <h2>Drawing Board </h2> 
-      <input 
-       type="string" 
-       name="text" 
-       value={text} 
-       onChange={SetOnchange}
-      ></input> 
-      <button 
-       onClick={SendMessage}
-      >Send</button> 
-      {messages.length !=0 && <div>
-          {messages.map((each)=> 
-          <p>{each}</p>)}
-         </div>}
-    </div>
-  </>;
+  const canvasRef = useRef<HTMLCanvasElement>(null) 
+   useEffect(()=>{
+     const ctx = canvasRef.current?.getContext("2d"); 
+     if(ctx!=null  && canvasRef.current != null)  { 
+       ctx.clearRect(0,0,canvasRef.current.width , canvasRef.current.height) 
+       ctx.fillStyle = "white"
+       ctx.fillRect(0,0,canvasRef.current.width , canvasRef.current.height) 
+     }
+   } , [])
+
+
+ const getMouseDown = (e : MouseEvent<HTMLCanvasElement>)=>{
+         const react  = canvasRef.current?.getBoundingClientRect(); 
+         if(!react) return ; 
+         SetPoints({
+            x :e.clientX - react.left , 
+            y : e.clientY - react.top 
+         })
+         SetisDrawing(true) ;
+ }
+
+ const DrewLine = ( ctx : CanvasRenderingContext2D | null | undefined,x2 : number , y2 :number )=>{ 
+  if(!ctx || !isDrawing || canvasRef.current == null) return ;  
+  ctx.clearRect(0, 0,canvasRef.current?.width, canvasRef.current.height); 
+  ctx.fillStyle = "white"
+  ctx.fillRect(0,0,canvasRef.current.width , canvasRef.current.height) 
+  ctx.strokeStyle = "black"; 
+  let w = x2-points.x ; 
+  let h = y2-points.y ; 
+  ctx.strokeRect(points.x , points.y , w , h);  
+ }
+ const getMouseMove = (e : MouseEvent<HTMLCanvasElement> ) =>{
+  if(!isDrawing) return ; 
+    const react = canvasRef.current?.getBoundingClientRect() ; 
+    if(!react) return ; 
+    const ctx = canvasRef.current?.getContext("2d"); 
+    const newX = e.clientX - react.left ; 
+    const newY = e.clientY - react.top ; 
+    DrewLine( ctx , newX , newY ); 
+ } 
+      return (
+        <>
+          <div className="text-center text-lg pt-4 ">Drawign Board</div>
+          <div>
+             <canvas  
+              ref = {canvasRef}
+              width={1000} 
+              height={1000} 
+              onMouseDown={getMouseDown}
+              onMouseMove={getMouseMove} 
+              onMouseUp={()=>SetisDrawing(false)}
+             ></canvas>
+          </div>
+        </>
+      )
 }
